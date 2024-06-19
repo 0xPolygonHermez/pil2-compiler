e3bcc81b12ae02a70a2609751cc5a33197c75450
zkronos73 <zkronos73@gmail.com>
2024-06-19 10:01:49 +0200

    WIP

---
+++ src/output.js
@@ -0,0 +1,47 @@
+
+class Output {
+    // setupPilOut
+    setup(options) {
+    }
+    setPublics(publics) {
+    }
+    setProofvalues(proofvalues) {
+    }
+    setChallenges(challenges) {
+    }
+    setGlobalConstraints(globalConstraints, packed) {
+    }
+    setGlobalExpressions(packed) {
+    }
+    setGlobalSymbols(symbols) {
+    }
+    encode() {
+    }
+    saveToFile(filename) {
+    }
+    useSubproof(id) {
+    }
+    setSubproof(id, name, options) { // options {aggregate}
+    }
+    setSubproofValues(subproofvalues) {
+    }
+    setAir(id, name, options) { // options {rows}
+    }
+
+    subproofProtoOut(subproofId, airId) {
+    }
+    setFixedCols(cols) {
+    }
+    setPeriodicCols(cols) {
+    }
+    setWitnessCols(cols) {
+    }
+    setConstraints(this.constraints, packed, options) {
+    }
+    setSymbolsFromLabels(labels, type, info) {
+    }
+    addHints(hints, packed) {
+    } 
+    setExpressions(packed) {
+    }
+}

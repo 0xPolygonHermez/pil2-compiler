2027c4c64a485c78dc33a74c8a76504dd733bd5e
zkronos73 <zkronos73@gmail.com>
2024-10-05 10:27:40 +0200

    fix optimization bug with big numbers inside fixeds

--- src/definition_items/fixed_col.js
+++ src/definition_items/fixed_col.js
@@ -3,6 +3,10 @@ const Context = require('../context.js');
 const fs = require('fs');
 const IntValue = require('../expression_items/int_value.js');
 // const Sequence = require("../sequence.js");
+
+const U64_MAX = 2n**64n - 1n;
+const BIG_INT = 16;
+
 module.exports = class FixedCol extends ProofItem {
     constructor (id, data) {
         super(id);
@@ -17,6 +21,9 @@ module.exports = class FixedCol extends ProofItem {
         this.fullFilled = false;
         this.buffer = null;
         this.converter = x => x;
+        this.currentSetRowValue = this.#setRowValue;
+        // TODO: more faster option, change function that call
+        // for each value to avoid verify if value is bigger than bytes specified
     }
     getId() {
         return this.id;
@@ -35,23 +42,27 @@ module.exports = class FixedCol extends ProofItem {
         if (value < 256n) return 1;
         if (value < 65536n) return 2;
         if (value < 4294967296n) return 4;
-        return 8;
+        if (value <= U64_MAX) return 8;
+        return BIG_INT; // big int 
     }
     createBuffer(rows, bytes) {
         const buffer = new Buffer.alloc(rows * bytes);
         switch (bytes) {
-            case 1: return [buffer, new Uint8Array(buffer, 0, rows), x => Number(x)];
-            case 2: return [buffer, new Uint16Array(buffer, 0, rows), x => Number(x)];
-            case 4: return [buffer, new Uint32Array(buffer, 0, rows), x => Number(x)];
-            case 8: return [buffer, new BigUint64Array(buffer, 0, rows), x => x];
+            case 1: return [buffer, new Uint8Array(buffer.buffer, 0, rows), x => Number(x)];
+            case 2: return [buffer, new Uint16Array(buffer.buffer, 0, rows), x => Number(x)];
+            case 4: return [buffer, new Uint32Array(buffer.buffer, 0, rows), x => Number(x)];
+            case 8: return [buffer, new BigUint64Array(buffer.buffer, 0, rows), x => x];
+            case BIG_INT: return [buffer, new Array(rows), x => x];
         }
         throw new Error(`invalid number of bytes ${bytes}`);
     }
-    checkIfResize(row, value) {
+    checkIfResize(row, value) {        
         switch (this.bytes) {
             case 1: if (value >= 256n) this.resizeValues(row, value); break;
             case 2: if (value >= 65536n) this.resizeValues(row, value); break;
             case 4: if (value >= 4294967296n) this.resizeValues(row, value); break;
+            case 8: if (value > U64_MAX) this.resizeValues(row, value); break;
+            case BIG_INT: return;
         }
     }
     setRowValue(row, value) {
@@ -61,6 +72,10 @@ module.exports = class FixedCol extends ProofItem {
         if (value && typeof value.asInt === 'function') {
             value = value.asInt();
         }
+        this.currentSetRowValue(row, value);
+    }
+    #setRowValue(row, value) {
+        value = Context.Fr.e(value);
         if (this.values === false){
             this.rows = Context.rows;
             if (this.bytes === false) {
@@ -68,16 +83,27 @@ module.exports = class FixedCol extends ProofItem {
             }
             [this.buffer, this.values, this.converter] = this.createBuffer(this.rows, this.bytes);
             this.size = this.rows * this.bytes;
+            this.updateSetRowValue();
+        } else {
+            this.checkIfResize(row, value);
         }
-        this.checkIfResize(row, value);
         if (row >= this.maxRow) this.maxRow = row;
         this.values[row] = this.converter(value);        
+    }
+    #fastSetRowValue(row, value) {
+        value = Context.Fr.e(value);
+        this.values[row] = this.converter(value);        
+    }
+    #ultraFastSetRowValue(row, value) {
+        value = Context.Fr.e(value);
+        this.values[row] = value;        
     }
     resizeValues(row, value) {
         let _bytes = this.valueToBytes(value);
         let [_buffer, _values, _converter] = this.createBuffer(this.rows, _bytes);
+        const _resizeConvert = this.bytes !== BIG_INT ? x => BigInt(x) : x => x;
         for (let i = 0; i < this.maxRow; ++i) { 
-            _values[i] = _converter(this.values[i]);
+            _values[i] = _resizeConvert(this.values[i]);
         }
         console.log(`\x1B[1;31mWARNING: fixed RESIZE from ${this.bytes} bytes to ${_bytes} on row ${row}/${this.maxRow} at ${Context.sourceRef}\x1B[0m`);    
         console.log(`\x1B[1;31muse #pragma fixed_bytes ${_bytes} to force initial size\x1B[0m`);    
@@ -86,6 +112,13 @@ module.exports = class FixedCol extends ProofItem {
         this.buffer = _buffer;
         this.converter = _converter;
         this.size = this.rows * this.bytes;
+        this.updateSetRowValue();
+    }
+    updateSetRowValue() {
+        const maxSizeValue = 2n ** BigInt(this.bytes * 8);
+        if (maxSizeValue > Context.Fr.p ) {
+            this.currentSetRowValue = this.bytes >= 8 ? this.#ultraFastSetRowValue : this.#fastSetRowValue;
+        }
     }
     getRowValue(row) {
         if (this.sequence) {

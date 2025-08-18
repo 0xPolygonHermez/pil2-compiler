459b6b574baf17fcfb14aaef13127b1f0d7912f8
zkronos73 <zkronos73@gmail.com>
2025-08-18 15:58:29 +0200

    add parametrer for fixed output

--- src/fixed_file.js
+++ src/fixed_file.js
@@ -19,8 +19,8 @@ module.exports = class FixedFile {
         this.valuesList[col] = values;
         this.labels[col] = label;
     }
-    saveToFile(filename) {                
-        const _filename = (!Context.outputDir || filename.startsWith('/')) ? filename : path.join(Context.outputDir, filename);
+    saveToFile(filename) {
+        const _filename = (!Context.fixedOutputDir || filename.startsWith('/')) ? filename : path.join(Context.fixedOutputDir, filename);
         const dirname = path.dirname(_filename);
         console.log(`  > Saving fixed file ${_filename} ...`);
         if (!fs.existsSync(dirname)) {
@@ -67,105 +67,4 @@ module.exports = class FixedFile {
         fs.closeSync(fd);
         return _filename;
     }    
-    static loadColumnFromFile(filename, col, rows, values, label) {
-        const fd = FixedFile.openFile(filename);
-        const cols = fd.size / (8 * rows);
-        console.log(`  > Loading fixed column ${label} on #${col} of file ${fd.filename} [size:${fd.size} rows:${rows} cols:${cols}]`);
-
-        const maxBuffSize = MAX_BUFF_SIZE; 
-        const totalSize = Math.min(cols * rows - col, fd.size / 8);
-        const buff = new BigUint64Array(Math.min(totalSize, maxBuffSize));
-        const buff8 = new Uint8Array(buff.buffer);
-
-        // First read (assume that first row is lower than maxBuffSize)
-
-        const length = Math.min(buff.length, totalSize) * 8;
-        const bytesRead = fs.readSync(fd, buff8, {offset: 0, position: 8 * col, length});
-        if (bytesRead < length) {
-            throw new Error(`Error reading file ${filename}, expected to read ${length} bytes, but got ${bytesRead}`);
-        }
-
-        // First row        
-        values[0] = BigInt(buff[0]);            
-
-        // Rest of rows
-        let index = cols;
-        for (let irow = 1; irow < rows; irow++) {
-            if (index >= buff.length) {
-                const p = irow * cols + col;
-                const length = Math.min(buff.length, totalSize + col - p) * 8;
-                // Read next chunk
-                const bytesRead = fs.readSync(fd, buff8, {offset: 0, position: 8 * p, length});
-                if (bytesRead < length) {
-                    throw new Error(`Error reading file ${filename}, expected to read ${length} bytes, but got ${bytesRead}`);
-                }
-                index = 0;
-            }
-            if (buff[index] === 'undefined') {
-                throw new Error(`Error reading file ${filename}, column ${col} is undefined [row:${irow}/${rows} index:${index} length:${length}]`);
-            }
-            values[irow] = BigInt(buff[index]);
-            index += cols;
-        }
-        fs.closeSync(fd);
-    }
-    /*
-    load() {
-        this.fullFilename = (!Context.config.inputDir || filename.startsWith('/')) ? filename : path.join(Context.config.inputDir, filename);
-        if (!fs.existsSync(this.fullFilename)) {
-            throw new Error(`Fixed file ${filename} (${this.fullFilename}) not found`);
-        }
-        this.fd = fs.openSync(this.fullFilename, "r");
-        const size = fs.fstatSync(fd).size;
-        this.loadHeader();
-
-        const airgroupName = fdBin.readString();
-        const airName = fdBin.readString();
-        const N = fdBin.readULE64();
-        const nFixedPols = await fdBin.readULE32();
-        const cols = fd.size / (8 * this.rows);
-        const maxBuffSize = MAX_BUFF_SIZE; 
-        const totalSize = cols * this.rows;
-        const buff = new BigUint64Array(Math.min(totalSize, maxBuffSize));
-        const buff8 = new Uint8Array(buff.buffer);
-        const load = new Array(cols).map((value, index) => this.loaded[index] !== true && typeof this.valuesList[index] !== 'undefined');
-
-        // First read (assume that first row is lower than maxBuffSize)
-
-        const length = Math.min(buff.length, totalSize) * 8;
-        const bytesRead = fs.readSync(fd, buff8, {offset: 0, position: 0, length});
-        if (bytesRead < length) {
-            throw new Error(`Error reading file ${filename}, expected to read ${length} bytes, but got ${bytesRead}`);
-        }
-
-        // First row
-        
-        for (let icol = 0; icol < cols; ++icol) {
-            if (load[icol]) {
-                continue;
-            }
-            this.loaded[icol] = true;
-            this.valuesList[icol][0] = BigInt(buff[icol]);            
-        }           
-
-        // Rest of rows
-        let p = cols;
-        for (let irow = 1; irow < this.rows; irow++) {
-            for (let icol = 0; icol < cols; ++icol) {
-                const index = p % buff.length;
-                if (index == 0) {
-                    const length = Math.min(buff.length, totalSize - p) * 8;
-                    const bytesRead = fs.readSync(fd, buff8, {offset: 0, position: p * 8, length});
-                    if (bytesRead < length) {
-                        throw new Error(`Error reading file ${filename}, expected to read ${length} bytes, but got ${bytesRead}`);
-                    }
-                }
-                if (load[icol]) {
-                    this.valuesList[icol][irow] = BigInt(buff[index]);
-                }
-                p += 1;
-            }            
-        }
-        fs.closeSync(fd);
-    }*/
 }
